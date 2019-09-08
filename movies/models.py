0628c94c9b2fd5ae970b18c418432982cde6dbc2
Marcin Koprek <marcin.koprek@infullmobile.com>
2019-09-08 15:49:51 +0200

    Solve DataError - value too long for type character varying(120)

--- movies/models.py
+++ movies/models.py
@@ -10,23 +10,23 @@ class Movie(TimeStampedModel):
     rated = models.CharField(max_length=10, null=True, blank=False)
     released = models.DateField(null=True, blank=False)
     runtime = models.CharField(max_length=10, null=True, blank=False)
-    genre = models.CharField(max_length=120, null=True, blank=False)
-    director = models.CharField(max_length=120, null=True, blank=False)
-    writer = models.CharField(max_length=120, null=True, blank=False)
+    genre = models.CharField(max_length=240, null=True, blank=False)
+    director = models.CharField(max_length=240, null=True, blank=False)
+    writer = models.CharField(max_length=240, null=True, blank=False)
     actors = models.CharField(max_length=240, null=True, blank=False)
     plot = models.TextField(null=True, blank=False)
-    language = models.CharField(max_length=120, null=True, blank=False)
-    country = models.CharField(max_length=120, null=True, blank=False)
-    awards = models.CharField(max_length=120, null=True, blank=False)
+    language = models.CharField(max_length=240, null=True, blank=False)
+    country = models.CharField(max_length=240, null=True, blank=False)
+    awards = models.CharField(max_length=240, null=True, blank=False)
     poster = models.URLField(null=True, blank=False)
     metascore = models.IntegerField(null=True, blank=False)
     imdbrating = models.FloatField(null=True, blank=False)
     imdbvotes = models.IntegerField(null=True, blank=False)
-    imdbid = models.CharField(max_length=120, null=True, blank=False)
-    typee = models.CharField(max_length=120, null=True, blank=False)  # 'type' is build-in name
+    imdbid = models.CharField(max_length=240, null=True, blank=False)
+    typee = models.CharField(max_length=240, null=True, blank=False)  # 'type' is build-in name
     dvd = models.DateField(null=True, blank=False)
-    boxoffice = models.CharField(max_length=120, null=True, blank=False)
-    production = models.CharField(max_length=120, null=True, blank=False)
+    boxoffice = models.CharField(max_length=240, null=True, blank=False)
+    production = models.CharField(max_length=240, null=True, blank=False)
     website = models.URLField(null=True, blank=False)
     response = models.BooleanField(null=True, blank=False)
     totalseasons = models.IntegerField(null=True, blank=True)
@@ -41,8 +41,8 @@ class Movie(TimeStampedModel):
 
 
 class Rating(TimeStampedModel):
-    source = models.CharField(max_length=120, null=True, blank=False)
-    value = models.CharField(max_length=120, null=True, blank=False)
+    source = models.CharField(max_length=240, null=True, blank=False)
+    value = models.CharField(max_length=240, null=True, blank=False)
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
 
     def __str__(self):
